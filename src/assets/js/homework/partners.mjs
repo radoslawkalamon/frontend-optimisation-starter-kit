dcf026c4787a8c4c635dab54dc47d6d4c233e66e
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2023-01-31 23:34:09 +0100

    feat: partners lottie on demand

--- src/assets/js/homework/partners.mjs
+++ src/assets/js/homework/partners.mjs
@@ -1,15 +1,52 @@
-import './vendor/lottie.min.js'
-
-const startAnimation = () => {
-  document.querySelectorAll('.animation').forEach((animation) => {		
-    lottie.loadAnimation({
-      wrapper: animation,
-      animType: 'svg',
-      loop: true,
-      path: animation.dataset.animationFile
-    });
-  });
+const helpers = {
+  initIsVisibleObserver: ({ callback, element, shallDisconnect = true }) => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        callback({ element, isVisible: entry.isIntersecting })
+        shallDisconnect && entry.isIntersecting && observer.disconnect()
+      })
+    })
+    observer.observe(element)
+  }
 }
 
-// Init Partners
-startAnimation();
+const animation = {
+  appendLottie: () => new Promise((resolve, reject) => {
+    const scriptEl = document.createElement('script')
+    scriptEl.src = '/assets/js/homework/vendor/lottie.min.js'
+    scriptEl.async = true
+    scriptEl.onload = () => resolve()
+    document.head.appendChild(scriptEl)
+  }),
+  fetchJSON: async () => {
+    const response = await fetch('/assets/videos/animation.json')
+    return response.json()
+  },
+  observerCallback: async ({ element, isVisible }) => {
+    if (isVisible) {
+      await animation.appendLottie()
+      const player = lottie.loadAnimation({
+        wrapper: element,
+        animType: 'svg',
+        loop: true,
+        animationData: await animation.fetchJSON()
+      })
+      player.setQuality('low')
+      player.addEventListener('DOMLoaded', () => {
+        helpers.initIsVisibleObserver({
+          callback: ({ isVisible }) => isVisible ? player.play() : player.stop(),
+          element,
+          shallDisconnect: false
+        })
+      })
+    }
+  },
+  init: () => {
+    helpers.initIsVisibleObserver({
+      callback: animation.observerCallback,
+      element: document.querySelector('.animation')
+    })
+  }
+}
+
+animation.init()
