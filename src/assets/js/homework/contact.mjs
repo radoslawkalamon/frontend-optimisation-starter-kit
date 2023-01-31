c78231549db07530268a4487dd1483b25f7e18ee
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2023-01-31 23:10:07 +0100

    feat: recaptcha loaded on scroll

---
+++ src/assets/js/homework/contact.mjs
@@ -0,0 +1,28 @@
+const helpers = {
+  initIsVisibleObserver: ({ callback, element, shallDisconnect = true }) => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        callback({ element, isVisible: entry.isIntersecting })
+        shallDisconnect && entry.isIntersecting && observer.disconnect()
+      });
+    });
+    observer.observe(element)
+  }
+}
+
+const contact = {
+  appendRecaptcha: () => {
+    const scriptEl = document.createElement('script')
+    scriptEl.src = 'https://www.google.com/recaptcha/api.js?render=6Lc-XPUZAAAAAOiLd3INhvxz6joup8GNDQ3Iy-uL'
+    scriptEl.async = true
+    document.head.appendChild(scriptEl)
+  },
+  setObserverForRecaptcha: () => {
+    helpers.initIsVisibleObserver({
+      callback: ({ isVisible }) => isVisible && contact.appendRecaptcha(),
+      element: document.querySelector('[data-hook="contact"]')
+    })
+  }
+}
+
+contact.setObserverForRecaptcha()
