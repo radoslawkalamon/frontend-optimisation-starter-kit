aa308d77bd17a5e5e8db739f4806b09bc8c5541c
Radosław Kalamon <kontakt+github@radoslawkalamon.pl>
2023-01-31 16:08:09 +0100

    feat: change $.ajax to fetch

--- src/assets/js/homework/news-listing.mjs
+++ src/assets/js/homework/news-listing.mjs
@@ -1,13 +1,9 @@
-import './vendor/jquery.min.js'
-
 const ctaAds = () => {
-  $.ajax({
-    url: '/cta-ad.html',
-    type: 'GET'
-  })
-    .then(function (response) {
+  fetch('/cta-ad.html')
+    .then(response => response.text())
+    .then(response => {
       document.getElementById('js-cta-ad').innerHTML = response;
-    });
+    })
 }
 
 // Init News Listing
